65e22c140b7f034ae0247371f3b67c07ddc64f9c
daniel mamuza <94477022+MamuzaD@users.noreply.github.com>
2026-02-28 02:38:26 -0800

    chore: `pnpm format`

--- apps/mobile/src/integrations/clerk/provider.tsx
+++ apps/mobile/src/integrations/clerk/provider.tsx
@@ -1,10 +1,6 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 
-export default function AppClerkProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AppClerkProvider({ children }: { children: React.ReactNode }) {
   return <ClerkProvider tokenCache={tokenCache}>{children}</ClerkProvider>;
 }
