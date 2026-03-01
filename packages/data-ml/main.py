58ab57f63ca8e7ebfd809291b46cfd812fb61128
Akigaii <reece.galgana@gmail.com>
2026-02-28 19:32:50 -0800

    feat: added query helper func for python files

--- packages/data-ml/main.py
+++ packages/data-ml/main.py
@@ -1,6 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+CONVEX_CLOUD_URL = os.getenv("CONVEX_CLOUD_URL")
+
+
+
+def query(table_name: str) -> dict:
+    response = requests.post(
+        f"{CONVEX_CLOUD_URL}/api/query",
+        json={"path": f"helpers:list",
+              "args": {"table_name": table_name}},
+    )
+    return response.json()
+
 def main():
-    print("Hello from data-ml!")
+    data = query("users")
+    data2 = query("comments")
+    print(data)
+    print(data2)
 
 
 if __name__ == "__main__":
     main()
+
