1be301c182bb76f6bc941195ca070805e11bf494
Akigaii <reece.galgana@gmail.com>
2026-03-01 14:02:07 -0800

    feat: collab filtering based off fake_** tables

--- packages/data-ml/main.py
+++ packages/data-ml/main.py
@@ -1,25 +1,65 @@
 import requests
 import os
-from dotenv import load_dotenv
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
+from convex import ConvexClient
+from dotenv import load_dotenv
 load_dotenv()
 CONVEX_CLOUD_URL = os.getenv("CONVEX_CLOUD_URL")
+client = ConvexClient(CONVEX_CLOUD_URL)
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def convex_clean(convex_data: dict) -> pd.DataFrame:
+    try:
+        # Extracts only numerical values used for collab. filtering.
+        df = pd.json_normalize(convex_data)
+        df = df.drop(columns = ['_id','_creationTime'])
+        return df
+    except:
+        raise Exception("ERROR: Convex data couldn't be cleaned.")
+
+
+def most_similar_users(df: pd.DataFrame, target_user: str, user_return_amt: int) -> list:
+    try:
+        # Compute similarity matrix and transform into dataframe.
+        user_similarity_np = cosine_similarity(df)
+        user_similarity_df = pd.DataFrame(user_similarity_np,     
+                                        index   = df.index,
+                                        columns = df.index )
+        # Sort top-simliar users and return new dataframe with such.
+        similar_users = (
+            user_similarity_df[target_user]
+            .drop(target_user)
+            .sort_values(ascending=False)
+            .head(user_return_amt))
+        similar_users = pd.DataFrame({"similarity_score": similar_users})
+        return similar_users
+    except:
+        raise Exception("ERROR: User similarity matrix could not be made.")
+    
 
 
 
-def query(table_name: str) -> dict:
-    response = requests.post(
-        f"{CONVEX_CLOUD_URL}/api/query",
-        json={"path": f"helpers:list",
-              "args": {"table_name": table_name}},
-    )
-    return response.json()
 
 def main():
-    data = query("users")
-    data2 = query("comments")
-    print(data)
-    print(data2)
+
+    # Deploy test/fake data tables.
+    client.mutation("seed:seed")
+
+    convex_data = client.query("helpers:list", {"table_name": "fake_user_attendance_100"})
+    df = convex_clean(convex_data)
+    df = df.set_index('user')
+    print(most_similar_users(df, "user10", 5))
+
+    convex_data = client.query("helpers:list", {"table_name": "fake_user_event_tags_vibe_100"})
+    df2 = convex_clean(convex_data)
+    df2 = df2.set_index('user')
+    print(most_similar_users(df2, "user10", 7))
 
 
 if __name__ == "__main__":
