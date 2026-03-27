504a7fb3d141f49f26f83658674c34a5e7c014c3
Nathan-Dela-Pena <nathan.mdelapena@gmail.com>
2026-03-27 00:40:18 -0700

    chore: added seed data & changed metro config to resolve imports

--- apps/mobile/src/app/(tabs)/(map)/index.tsx
+++ apps/mobile/src/app/(tabs)/(map)/index.tsx
@@ -1,24 +1,12 @@
 import { useUserLocation } from '@/features/map/hooks/use-user-location';
 import { coordsToH3Cell, pointsToGeoJSON } from '@/features/map/utils/h3';
+import { eventSeedAttendees, eventSeeds } from '@fomo/backend/convex/seed';
 import MapboxGL from '@rnmapbox/maps';
 import { useRouter } from 'expo-router';
 import type { Point } from 'geojson';
 import { useMemo, useRef } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
-// replace with reformatted schema data soon
-const tempMockData = [
-  { longitude: -115.19299595922035, latitude: 36.12730186736902, weight: 14 }, // Coffee + Homework (Pop Cafe)
-  { longitude: -115.16859227452814, latitude: 36.10470771447518, weight: 55 }, // ASAP Rocky Concert
-  { longitude: -115.14269190902489, latitude: 36.10790291877858, weight: 31 }, // Psi Rho house party
-  { longitude: -115.15239854806732, latitude: 36.15958759998514, weight: 42 }, // Las Vegas - First Friday
-  { longitude: -115.18328464909499, latitude: 36.09086216508982, weight: 18 }, // St. Jimmy Panel & Conference
-  { longitude: -115.15149229313623, latitude: 36.12871445143533, weight: 37 }, // LVL UP EXPO 2026
-  { longitude: -115.17538973403965, latitude: 36.10987909759377, weight: 48 }, // Baby Keem Concert
-  { longitude: -115.24699453074136, latitude: 36.03809903957797, weight: 9 }, // Water Lantern Festival
-  { longitude: -115.15269829421878, latitude: 36.15909747099756, weight: 24 }, // Thrift Valley pop up shop
-];
 
 MapboxGL.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN ?? '');
 
@@ -28,7 +16,10 @@ export default function MapScreen() {
   const cameraRef = useRef<MapboxGL.Camera>(null);
   const { centerCoordinate, hasResolvedLocation, locationGranted } = useUserLocation();
 
-  const heatmapGeoJSON = useMemo(() => pointsToGeoJSON(tempMockData), []);
+  const heatmapGeoJSON = useMemo(
+    () => pointsToGeoJSON(eventSeeds.map((e, i) => ({ ...e, weight: eventSeedAttendees[i] ?? 1 }))),
+    []
+  );
 
   return (
     <View className="absolute inset-0">
@@ -62,7 +53,7 @@ export default function MapScreen() {
           <MapboxGL.HeatmapLayer
             id="activity-heat"
             style={{
-              heatmapWeight: ['interpolate', ['linear'], ['get', 'weight'], 0, 0, 55, 1],
+              heatmapWeight: ['interpolate', ['linear'], ['get', 'weight'], 0, 0, 6, 1],
               heatmapIntensity: ['interpolate', ['linear'], ['zoom'], 10, 1, 15, 3],
               heatmapColor: [
                 'interpolate',
