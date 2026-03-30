cfe57df4740a156447f3b4d81fdc473f1580b1ee
HristiyanMelios <124943450+HristiyanMelios@users.noreply.github.com>
2026-03-29 23:37:39 -0700

    refactor(backend): fix all queries to use new folder structure

--- packages/data-ml/friendRecs.py
+++ packages/data-ml/friendRecs.py
@@ -22,18 +22,18 @@ def get_client() -> ConvexClient:
 
 # Checks if a userid exists in the "users" table.
 def user_exists(user_id: str) -> bool:
-    return get_client().query("query:userId", {"userId": user_id}) is not None
+    return get_client().query("data_ml/users:userExists", {"userId": user_id}) is not None
     
 
 # Combines "usersToEvents" and "events" into a single dataframe.
 def join_user_events() -> pd.DataFrame:
 
     # Store "usersToEvents" and "events" into dataframes.
-    usersToEvents_data = get_client().query("query:list", {"table_name": "usersToEvents"})
+    usersToEvents_data = get_client().query("data_ml/universal:queryAll", {"table_name": "usersToEvents"})
     usersToEvents_df = pd.json_normalize(usersToEvents_data)
     usersToEvents_df = usersToEvents_df[["eventId", "userId"]]
 
-    events_data = get_client().query("query:list", {"table_name": "events"})
+    events_data = get_client().query("data_ml/universal:queryAll", {"table_name": "events"})
     events_df = pd.json_normalize(events_data)
     events_df = events_df[["_id", "name"]]
 
@@ -62,11 +62,11 @@ def raw_matrix_eventTags() -> pd.DataFrame:
     user_events_df = join_user_events()
 
     # Join "events" and "tags" dataframes.
-    eventTags_json = get_client().query("query:list", {"table_name": "eventTags"})
+    eventTags_json = get_client().query("data_ml/universal:queryAll", {"table_name": "eventTags"})
     eventTags_df = pd.json_normalize(eventTags_json)
     eventTags_df = eventTags_df[["eventId", "tagId"]]
 
-    tags_json = get_client().query("query:list", {"table_name": "tags"})
+    tags_json = get_client().query("data_ml/universal:queryAll", {"table_name": "tags"})
     tags_df = pd.json_normalize(tags_json)
     tags_df = tags_df[["_id", "name"]]
 
@@ -88,11 +88,11 @@ def raw_matrix_eventTags() -> pd.DataFrame:
 def raw_matrix_postTags() -> pd.DataFrame:
 
     # Join "users" and "posts" dataframes.
-    users_json = get_client().query("query:list", {"table_name": "users"})
+    users_json = get_client().query("data_ml/universal:queryAll", {"table_name": "users"})
     users_df = pd.json_normalize(users_json)
     users_df = users_df[["_id"]]
 
-    posts_json = get_client().query("query:list", {"table_name": "posts"})
+    posts_json = get_client().query("data_ml/universal:queryAll", {"table_name": "posts"})
     posts_df = pd.json_normalize(posts_json)
     posts_df = posts_df[["_id", "authorId"]]
 
@@ -101,11 +101,11 @@ def raw_matrix_postTags() -> pd.DataFrame:
     users_posts_df = users_posts_df.rename(columns={"_id_x": "user_id"})
 
     # Join "postTags" and "tags" dataframes
-    postTags_json = get_client().query("query:list", {"table_name": "postTags"})
+    postTags_json = get_client().query("data_ml/universal:queryAll", {"table_name": "postTags"})
     postTags_df = pd.json_normalize(postTags_json)
     postTags_df = postTags_df[["postId", "tagId"]]
 
-    tags_json = get_client().query("query:list", {"table_name": "tags"})
+    tags_json = get_client().query("data_ml/universal:queryAll", {"table_name": "tags"})
     tags_df = pd.json_normalize(tags_json)
     tags_df = tags_df[["_id", "name"]]
 
@@ -170,7 +170,7 @@ def upsert_friend_recs(sim_scores: pd.DataFrame, userId: str, rec_amt: int) -> N
     top_sim_scores = [
         {"userId": friendId, "score": float(score)}
         for friendId, score in top_sim_scores["similarity_score"].items() 
-        if get_client().query("query:friend_exists", {"userAId": userId, "userBId": friendId}) is None
+        if get_client().query("data_ml/friends:friendExists", {"userAId": userId, "userBId": friendId},) is None
     ]
 
     # If list is larger than rec_amt, truncate.
@@ -178,9 +178,10 @@ def upsert_friend_recs(sim_scores: pd.DataFrame, userId: str, rec_amt: int) -> N
     top_sim_scores = top_sim_scores[:rec_amt]
     
     # Add row if user doesn't have any recommended friends, if they do, update names if values changed.
-    get_client().mutation("friendRecs:upsert", {"userId": userId,
-                                          "recs": top_sim_scores
-                                          })  
+    get_client().mutation(
+        "data_ml/friendRecs:upsert",
+        {"userId": userId, "recs": top_sim_scores},
+    )
 
 
 
