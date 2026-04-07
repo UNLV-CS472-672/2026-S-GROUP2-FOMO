b0ef8eb92673a93a3a7855245d6667da73293da4
daniel mamuza <94477022+MamuzaD@users.noreply.github.com>
2026-04-06 19:50:47 -0700

    refactor(mobile): remove search screen

--- apps/mobile/src/app/(tabs)/(map)/_layout.tsx
+++ apps/mobile/src/app/(tabs)/(map)/_layout.tsx
@@ -4,7 +4,6 @@ export default function MapLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="search" options={{ title: 'Search', animation: 'none' }} />
     </Stack>
   );
 }
