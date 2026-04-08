ee89bca33de1f1dc074bfc126aabb01ca1ff5e11
Akigaii <reece.galgana@gmail.com>
2026-04-08 12:52:58 -0700

    refactor: changed seed.ts to requester/refactor/status

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -89,7 +89,7 @@ export default defineSchema({
   friends: defineTable({
     requesterId: v.id('users'),
     recipientId: v.id('users'),
-    status: v.union(v.literal('pending'), v.literal('accepted'), v.literal('rejected')),
+    status: v.union(v.literal('pending'), v.literal('accepted'), v.literal('ignored')),
   })
     .index('by_requesterId', ['requesterId'])
     .index('by_recipientId', ['recipientId'])
