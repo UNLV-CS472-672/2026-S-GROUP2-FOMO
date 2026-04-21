d612e96e017edab785e6c791781568c805c573db
sandhm1 <manjotssandhu1@gmail.com>
2026-04-21 14:32:35 -0700

    feat(backend): I forgot to remove utils

--- packages/data-ml/event-rec/recommendEvent.py
+++ packages/data-ml/event-rec/recommendEvent.py
@@ -183,7 +183,4 @@ TODO:
                  Idk how to really do that with a running weight adjustment though
     3. (recommendEvent.py) Need to fix day_norm, hour_norm, and is_free fields in get_event_features. Commented "FIX:" at the spot
     4. (recommendEvent.py) Need to push recs to DB
-    5. (All) UNIT TESTS
-    6. (Optional): updateUserPreferences and this file may have some repetitive code, could add to utils file. If nothing needs to be added there
-                   then we should just remove utils. Theres nothing really of use in there right now.
 """
