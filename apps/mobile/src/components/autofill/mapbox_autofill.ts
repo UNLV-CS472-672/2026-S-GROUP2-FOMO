30f707db4f1a7719c6b1023fcf31157fead31c2b
Akigaii <reece.galgana@gmail.com>
2026-04-26 20:30:09 -0700

    feat: added debouncer

--- apps/mobile/src/components/autofill/mapbox_autofill.ts
+++ apps/mobile/src/components/autofill/mapbox_autofill.ts
@@ -1,1 +1,10 @@
-// Bazinga!
+function debounce<T extends (...args: unknown[]) => void>(fn: T, delay: number = 300) {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<T>): void => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+      timer = null;
+    }, delay);
+  };
+}
